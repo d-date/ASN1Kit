2fe7b30833204d793b758ffd43548e3f565c1a4b
Daiki Matsudate <d.matsudate@gmail.com>
2022-04-04 01:22:10 +0900

    update to swift 5.6

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -27,18 +27,20 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "http://github.com/Quick/Nimble", from: "9.2.0"),
         .package(url: "https://github.com/SwiftCommon/DataKit.git", from: "1.1.0"),
-        .package(url: "http://github.com/Carthage/Commandant", from: "0.17.0"),
-        .package(name: "GemCommonsKit", url: "https://github.com/gematik/ref-GemCommonsKit", .branch("1.2.0"))
+        .package(url: "http://github.com/Carthage/Commandant", from: "0.18.0"),
+        .package(url: "https://github.com/gematik/ref-GemCommonsKit", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ASN1Kit",
-            dependencies: ["DataKit", "GemCommonsKit"]),
-        .target(
+            dependencies: ["DataKit",
+                           .product(name: "GemCommonsKit", package: "ref-GemCommonsKit")
+                          ]),
+        .executableTarget(
             name: "CLI",
-            dependencies: ["ASN1Kit", "Commandant"]),
+            dependencies: ["ASN1Kit", .product(name: "Commandant", package: "Commandant")]),
         .testTarget(
             name: "ASN1KitTests",
             dependencies: ["ASN1Kit", "Nimble"]),
